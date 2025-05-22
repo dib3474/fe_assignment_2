295e3e821a77efa0a089a2c32d0683951ff0293a
dib3474 <dib3474@gmail.com>
2025-05-23 00:13:11 +0900

    Toast 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,37 @@ import { createContext, useState } from "react";
 import Home from './pages/Home'
 import Dex from './pages/Dex'
 import Detail from './pages/Detail'
+import { ToastContainer, toast } from 'react-toastify';
 
 export const PokemonContext = createContext(null);
 
+const ErrorMsg = (msg) => {
+    toast.error(msg, {
+        position: 'top-center',
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+    });
+};
+
 const App = () => {
   const [selectPokemon, setSelectPokemon] = useState([]);
-  const addPokemon = async(pokemon) => {
+  const addPokemon = (pokemon) => {
     if (selectPokemon.length === 6) {
-      alert("더 이상 선택할 수 없습니다.");
+      ErrorMsg("더 이상 선택할 수 없습니다.");
       return ;
     }
     if (selectPokemon.find((element) => element === pokemon)) {
-      alert("이미 선택된 포켓몬입니다.");
+      ErrorMsg("이미 선택된 포켓몬입니다.");
       return ;
     }
     setSelectPokemon([...selectPokemon, pokemon]);
   }
-  const removePokemon = async(pokemon) => {
+  const removePokemon = (pokemon) => {
     setSelectPokemon(selectPokemon.filter((dex)=> {
       if (pokemon.id !== dex.id) return dex;
     }));
@@ -33,6 +47,7 @@ const App = () => {
             <Route path="/detail/:id" element={<Detail />}/>
         </Routes>
       </BrowserRouter>
+      <ToastContainer />
     </PokemonContext.Provider>
   )
 }
